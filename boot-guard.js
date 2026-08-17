e1b62ee276b0bfa3bdcfa6ffcb8ba7255df34142
inlasco <niconavarro@me.com>
2026-08-17 08:42:45 +0200

    build: update launcher bridge cache integrity size

--- boot-guard.js
+++ boot-guard.js
@@ -47,7 +47,7 @@
     "game-v13.4-fr.love": 3479845,
     "11.5/love.wasm": 4720510,
     "lua/normalize1.lua": 2832,
-    "lua/normalize2.lua": 4095
+    "lua/normalize2.lua": 14528
   };
 
   var PLAYER_SRC =
